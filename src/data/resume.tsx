15b2dfb3ff967b693e342e294e3ee8c468a7d926
Kafka <664945264@qq.com>
2024-11-09 23:29:11 +0800

    blog v1

---
+++ src/data/resume.tsx
@@ -0,0 +1,9 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+}
